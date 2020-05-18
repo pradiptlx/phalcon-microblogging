952b567ed21a62fd8738405f742cec8df4c00457
Lix <prdptlx@gmail.com>
2020-05-18 21:07:39 +0700

    Dump SQL

---
+++ dump/phalcon_microblog_dbo_reply.sql
@@ -0,0 +1,6 @@
+insert into dbo.reply (id, post_id, original_post_id) values (2, N'be6301f8-ce52-4fe7-bfb5-639c8e3692b4', N'254986D8-BD0E-4D89-A3D3-E2989596FF07');
+insert into dbo.reply (id, post_id, original_post_id) values (3, N'99f98b0d-fdb4-45e0-a44d-e8791fdbb412', N'BE6301F8-CE52-4FE7-BFB5-639C8E3692B4');
+insert into dbo.reply (id, post_id, original_post_id) values (4, N'e84ca263-6377-4ff8-a2a8-ec2c425760d4', N'99F98B0D-FDB4-45E0-A44D-E8791FDBB412');
+insert into dbo.reply (id, post_id, original_post_id) values (5, N'd37cc27c-f002-4e2a-9c5f-e2650b6ac799', N'254986D8-BD0E-4D89-A3D3-E2989596FF07');
+insert into dbo.reply (id, post_id, original_post_id) values (6, N'49a62658-43e5-4851-8837-2ab0c9d529e4', N'254986D8-BD0E-4D89-A3D3-E2989596FF07');
+insert into dbo.reply (id, post_id, original_post_id) values (7, N'5b220b54-8a60-4a54-b613-2a095cde022c', N'254986D8-BD0E-4D89-A3D3-E2989596FF07');
